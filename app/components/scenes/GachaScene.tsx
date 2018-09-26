d2cd776bd7593fe20320b064438e6969c3f17554
ninislope <reg@narazaka.net>
2018-09-26 23:13:25 +0900

    ホームに戻る

--- app/components/scenes/GachaScene.tsx
+++ app/components/scenes/GachaScene.tsx
@@ -7,7 +7,7 @@ export const GachaScene = observer(({store}: BaseProps) => (
     <div className={scene}>
         <span className="title" style={center({top: "30%", height: "1.5em"}, {fontSize: "2em"})}>ガチャ</span>
         <Button style={center({top: 70, left: 30, height: "4em", width: "4em"}, {fontSize: "2em", background: "#ff3"})}
-            onClick={() => store.SceneManager.goto("gacha")}>ガチャ</Button>
+            onClick={() => store.SceneManager.goto("home")}>ガチャ</Button>
         <Button style={center({top: 70, left: 50, height: "4em", width: "4em"}, {fontSize: "2em", background: "#f33"})}
             onClick={() => store.SceneManager.goto("gacha")}>闘技場</Button>
     </div>
