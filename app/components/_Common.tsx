84ea3f583fbcc7c61bdfaf39165b77e62b1ba983
ninislope <reg@narazaka.net>
2018-09-27 22:41:52 +0900

    common

--- app/components/_Common.tsx
+++ app/components/_Common.tsx
@@ -2,10 +2,17 @@ import * as React from "react";
 import { Store } from "../states/Store";
 import { Style } from "./style";
 
-const button = Style.registerStyle({ border: "none", fontSize: "1rem" });
+const button = Style.registerStyle({
+    position: "absolute",
+    border: "none",
+    fontSize: "1rem",
+    cursor: "pointer",
+
+});
 
 export interface BaseProps {
     store: Store;
+    children?: React.ReactNode;
 }
 
 export const Button: React.SFC<React.HTMLAttributes<HTMLButtonElement>> = (props) =>
