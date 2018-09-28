1bac28602145d9ebb77691492cd1b9d2be7215e1
ninislope <reg@narazaka.net>
2018-09-29 01:02:56 +0900

    ガチャ関係

--- app/components/scenes/GachaScene.tsx
+++ app/components/scenes/GachaScene.tsx
@@ -2,10 +2,29 @@ import * as React from "react";
 import { observer } from "mobx-react";
 import { LargeButton, BaseProps } from "../Common";
 import { UIScene } from "./UIScene";
+import * as Models from "../../states/MasterData/index";
 
 export const GachaScene = observer(({store}: BaseProps) => (
     <UIScene title="ガチャ" store={store}>
-        <LargeButton top={70} right={30}
-            onClick={store.callGoto("home")}>ガチャ</LargeButton>
+        <div style={{position: "absolute", top: "10%", left: "5%"}}>
+            <h2>開催中のガチャ</h2>
+            {
+                Models.$GachaSchedule.inTime().map(gachaSchedule =>
+                    <div>
+                        <span>{gachaSchedule.typeGacha().name}</span>
+                        <span>
+                            {
+                                gachaSchedule.typeGacha().gacha.drawTypes.map(drawType =>
+                                    <div>
+                                        <span>{drawType.gachaDrawTypeName.name} : {drawType.requireItem.name} ✕ {drawType.requireItemAmount}</span>
+                                        <LargeButton relative={true} top={0} right={0} onClick={store.callGoto("home")}>ガチャ</LargeButton>
+                                    </div>
+                                )
+                            }
+                        </span>
+                    </div>
+                )
+            }
+        </div>
     </UIScene>
 ));
--- app/states/MasterData/GachaSchedule.tsx
+++ app/states/MasterData/GachaSchedule.tsx
@@ -2,8 +2,22 @@ import * as Models from "./Models";
 import { applyRecordExtension } from "./applyRecordExtension";
 
 export class GachaScheduleExt {
+    inTime(this: Models.GachaSchedule, now = new Date()) {
+        return (!this.startAt || this.startAt <= now) && (!this.endAt || now < this.endAt);
+    }
+
+    typeGacha(this: Models.GachaSchedule) {
+        switch(this.gachaType) {
+            case "SingleGacha": return Models.$SingleGacha.find(this.gachaId);
+            // case "StepUpGacha": return Models.$StepUpGacha.find(this.gachaId);
+            default: throw new Error("unknown gacha type!");
+        }
+    }
 }
 export class GachaScheduleListExt {
+    inTime(this: Models.GachaScheduleList, now = new Date()) {
+        return this.records.filter(record => record.inTime(now));
+    }
 }
 
 applyRecordExtension(Models.GachaSchedule, GachaScheduleExt);
