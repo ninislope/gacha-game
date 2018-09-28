c952a8162a99388344feef88ad848962d733be2a
ninislope <reg@narazaka.net>
2018-09-28 20:37:22 +0900

    user data

--- app/states/UserData/UserActor.tsx
+++ app/states/UserData/UserActor.tsx
@@ -2,11 +2,18 @@ import * as Models from "../MasterData/index";
 import { observable, computed } from "mobx";
 import { Savable } from "./Savable";
 
-export class UserActor extends Savable<UserActor> {
-    @observable actorId!: number;
+export class UserActorProps extends Savable<UserActorProps> {
     @observable level!: number;
+    @observable actorId!: number;
+    @observable equipmentIds!: number[];
+}
 
+export class UserActor extends UserActorProps {
     @computed get actor() {
         return Models.$Actor.find(this.actorId);
+    }
+
+    @computed get equipments() {
+        return Models.$Equipment.findAll(this.equipmentIds);
     }
 }
