10ffe2f4ec9928f9642544e5dbfbad9a1c197e6f
ninislope <reg@narazaka.net>
2018-09-28 13:55:50 +0900

    load master data

--- app/states/Store.tsx
+++ app/states/Store.tsx
@@ -2,6 +2,8 @@ import { SceneManager, SceneId } from "./SceneManager";
 import { observable, action } from "mobx";
 import { User } from "./UserData/User";
 import { autosave, load } from "./autosave";
+import * as Models from "./MasterData/index";
+import { BaseRecordList } from "./MasterData/BaseRecordList";
 
 export class Store {
     @observable SceneManager = new SceneManager();
@@ -11,10 +13,25 @@ export class Store {
     @action async load() {
         if (!this.loadState) this.loadState = "loading";
         await wait();
+
+        try {
+            await this.loadMasterData();
+        } catch (e) { console.error(e); }
+
         const user = load("user");
         if (user) this.User = User.fromJson(user) as any;
         autosave("user", this.User);
         this.loadState = "loaded";
+    }
+
+    loadMasterData() {
+        return Promise.all(
+            Object.
+                keys(Models).
+                filter(name => name.startsWith("$")).
+                map(name => (Models as any)[name] as BaseRecordList<any, any>).
+                map((model) => model.load()),
+        );
     }
 
     @action destroy() {
